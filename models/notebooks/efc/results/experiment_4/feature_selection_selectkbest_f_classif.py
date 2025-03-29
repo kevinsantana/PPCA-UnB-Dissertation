dda312585528a0cb8519f6af04d1c907059b2d33
Kevin Araujo <kevin_santana.araujo@hotmail.com>
2025-03-29 20:26:35 -0300

    update experiment 4 to be consistent with reamaning experiments set up

--- models/notebooks/efc/results/experiment_4/feature_selection_selectkbest_f_classif.py
+++ models/notebooks/efc/results/experiment_4/feature_selection_selectkbest_f_classif.py
@@ -1,9 +1,10 @@
-from pprint import pprint
+from typing import Optional
 
 import pandas as pd
 from efc import EnergyBasedFlowClassifier
 from results.common.constants import (
     CUTOFF_QUANTILE,
+    EFC_CLF,
     LAST_TIME_STEP,
     LAST_TRAIN_TIME_STEP,
     N_BINS,
@@ -25,6 +26,7 @@ from sklearn.feature_selection import SelectKBest, f_classif
 def make_feature_selection_with_k_best(k: int,
                                        fig_folder: str,
                                        fig_name: str,
+                                       technique: Optional[str] = None,
                                        X: pd.DataFrame = None,
                                        y: pd.Series = None,
                                        return_x_y: bool = False
@@ -44,26 +46,48 @@ def make_feature_selection_with_k_best(k: int,
     if return_x_y:
         return X_new, y
 
-    sizes = get_dataset_size(f"Feature Selection Score Function f_classif k={k}", X_train, X_test, y_train, y_test)
-    clf = EnergyBasedFlowClassifier(n_bins=N_BINS, cutoff_quantile=CUTOFF_QUANTILE, pseudocounts=PSEUDOCOUNTS)
+    clf = EFC_CLF
     clf.fit(X_train, y_train)
     y_pred, y_energies = clf.predict(X_test, return_energies=True)
-    plot_efc_energies(clf, y_test, y_energies, fig_folder, fig_name)
-    model_score = calculate_model_score(technique=f"Feature Selection Score Function f_classif k={k}", y_true=y_test.values, y_pred=y_pred)
-    confusion_matrix_values = custom_confusion_matrix(technique=f"Feature Selection Score Function f_classif k={k}",
-                                                      y_test=y_test,
-                                                      y_pred=y_pred)
-    return sizes, model_score, confusion_matrix_values
+    plot_efc_energies(
+        clf=clf,
+        y_test=y_test,
+        y_energies=y_energies,
+        fig_name=fig_name,
+        fig_folder=fig_folder,
+    )
+    sizes = get_dataset_size(
+        technique=technique,
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test
+    )
+    metric_dict = calculate_model_score(
+        technique=technique, y_true=y_test.values, y_pred=y_pred
+    )
+    confusion_matrix = custom_confusion_matrix(
+        technique=technique, y_test=y_test, y_pred=y_pred
+    )
+
+    return sizes, metric_dict, confusion_matrix
 
 
-def make_feature_selection_with_k_best_no_agg_features(k: int,
-                                                       fig_folder: str,
-                                                       fig_name: str) -> None:
+def make_feature_selection_with_k_best_no_agg_features(
+        k: int,
+        fig_folder: str,
+        fig_name: str,
+        technique: Optional[str] = None
+    ) -> None:
     X_train, X_test, y_train, y_test = drop_agg_features()
     X, y = train_test_from_splitted(X_train, y_train, X_test, y_test, return_df=False)
-    sizes, model_score, confusion_matrix_values = make_feature_selection_with_k_best(k,
-                                                                                     fig_folder,
-                                                                                     fig_name,
-                                                                                     X=X,
-                                                                                     y=y)
+    sizes, model_score, confusion_matrix_values = make_feature_selection_with_k_best(
+        technique=technique,
+        k=k,
+        fig_folder=fig_folder,
+        fig_name=fig_name,
+        X=X,
+        y=y
+    )
+
     return sizes, model_score, confusion_matrix_values
